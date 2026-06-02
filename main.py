7bb91014947dc9d0be94fa6153eb5e91bf49a6c7
andrefetch <andresilvapersonal2007@gmail.com>
2026-06-02 10:09:05 -0400

    Can work in files and has added agentic flow using a for loop.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 from prompts import system_prompt
 from dotenv import load_dotenv
@@ -6,6 +7,7 @@ from google import genai
 from google.genai import types
 from functions.call_function import available_functions
 from functions.get_files_info import schema_get_files_info
+from functions.call_function import call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -37,30 +39,48 @@ config = types.GenerateContentConfig(
     temperature=0
 )
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash',
-    contents=messages,
-    config=config,
-)
-
-if response.usage_metadata is None:
-    raise RuntimeError("Prompt is empty, or there is an internal error.")
-
 '''
 Function Definitions
 '''
 
 def main() -> str:
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}\n")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
 
-    if not response.function_calls:
-        print(f"{response.text}")
-    else:
+    for _ in range(20):
+        response = client.models.generate_content(
+            model='gemini-2.5-flash',
+            contents=messages,
+            config=config,
+    )  
+
+        if response.usage_metadata is None:
+            raise RuntimeError("Prompt is empty, or there is an internal error.")
+        
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
+        if not response.function_calls:
+            print(f"{response.text}")
+            break
+
+        function_response_parts = []
+
         for func_calls in response.function_calls:
-            print(f"Calling function: {func_calls.name}({func_calls.args})")
+
+            function_call_result = call_function(func_calls, args.verbose)
+
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+
+            if (function_call_result.parts) and (function_call_result.parts[0].function_response) and (function_call_result.parts[0].function_response.response):
+                function_response_parts.append(function_call_result.parts[0])
+            else:
+                raise Exception("Error: Function call is not operating correctly.")
+        
+        messages.append(types.Content(role="user", parts=function_response_parts))
+    
+    else:
+        print("Maximum iterations reached without a final response.")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
